00f969db391f2abd0362f8608fc7efdfce1d1c21
Joao Gabriel <jdacostaribeiro94@gmail.com>
2025-06-09 23:58:21 -0300

    start_cmd_menu

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from enfermeiro import Enfermeiro
 from cliente import Cliente
 from agendamento import Agendamento
 
+funcionarios = []
+clientes = []
+agendamentos = []
+
 # Criar profissionais
 dentista1 = Dentista("Dra. Ana", 101, "Ortodontia")
 enfermeiro1 = Enfermeiro("Carlos", 202, "Pediatria")
@@ -20,10 +24,57 @@ dentista1.agendamentos.append(ag1)
 enfermeiro1.agendamentos.append(ag2)
 
 # Exibir agendamentos dos funcionários
-print(f"Agendamentos do dentista {dentista1.nome}:")
-for ag in dentista1.agendamentos:
-    print(f"- {ag.cliente.nome} em {ag.data} às {ag.horario}")
 
-print(f"\nAgendamentos do enfermeiro {enfermeiro1.nome}:")
-for ag in enfermeiro1.agendamentos:
-    print(f"- {ag.cliente.nome} em {ag.data} às {ag.horario}")
+def menu ():
+  
+  print('-----------------------------------')
+  print('------SISTEMA DE AGENDAMENTOS------')
+  print('-----------------------------------\n')
+  print('1- Cadastrar funcionario')
+  print('2- Cadastrar cliente')
+  print('3- Fazer agendamento')
+  print('4- Sair\n')
+
+
+def fun_cad ():
+  
+  print('------CADASTRO DE FUNCIONARIO------\n')
+  print('Qual o tipo:')
+  print('1- Dentista')
+  print('2- Enfermeiro\n')
+  tipo_fun = input('Escolha uma das opções: ')
+  
+  nome = input('Digite o nome do funcinario:')
+
+  id_fun = input('Qual o id do funcionario:')
+
+  if tipo_fun == '1':
+    esp = input('Qual a especialidade do(a) dentista: ')
+    funcionario = Dentista(nome, id_fun, esp)
+  
+  elif tipo_fun == '2':
+    setor = input('Qual o setor do(a) enfermeiro(a): ')
+    funcionario = Enfermeiro(nome, id_fun, setor)
+
+  else:
+    print('Tipo invalido')
+    return
+  
+  funcionarios.append(funcionario)
+  print('Funcionario cadastrado com sucesso\n')
+
+
+while True:
+  menu()
+  opcao = input('Escolha uma opção: \n')
+
+  if opcao == '4':
+    print('Obrigado por usar nosso sitema...')
+    break
+  
+  elif opcao == '1':
+    fun_cad()
+
+  else:
+    print('Opção invalida, tente novamente')
+  
